fb6ebc40f5c6d8c4edf66e747b9081ea006f76fe
sanjyotgund9047 <sanjyotgund6@gmail.com>
2025-09-11 02:47:03 +0530

    Initial commit

---
+++ JS/code/datatypes/index.js
@@ -0,0 +1,57 @@
+let a = 25;
+console.log(typeof a)
+a = "prasad"
+console.log(typeof a);
+a = true;
+console.log(typeof a);
+a = undefined;
+console.log(typeof a);
+a = null;
+console.log(typeof a);
+a = [1, 2];
+console.log(typeof a);
+a = {};
+console.log(typeof a); 
+function x () { }
+console.log(typeof x);
+
+
+// numbers
+let num = 25.4118;
+console.log(typeof num) //number
+console.log(num.toFixed(2)) // it will round up for 2 digits after decimal 25.41
+console.log(num.toPrecision(3)) // it will round up for 3 digits from starting  25.4
+num = 25;
+console.log(num.toString(2)) //11001 tostring convert number into string of any base
+
+// math methods
+
+console.log(Math.sqrt(16)) //4
+console.log(Math.cbrt(27)) //3
+console.log(Math.pow(2, 3)) //8
+console.log(Math.abs(-10)) //10
+console.log(Math.max(5, 3, 2, 1, 7, 8, 9, 12)) //12
+console.log(Math.min(5, 3, 2, 1, 7, 8, 9, 12)) //1
+console.log(Math.ceil(2.1)) //3 because 3 is nearest greater value
+console.log(Math.floor(2.8)) //2 because 2 is nearest smaller value
+console.log(Math.round(2.6)) //3 because nearest value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/code/first/script.js
@@ -0,0 +1,22 @@
+/* This is 
+a multi
+line comment */
+// This is a single line comment
+// var a = 5;
+// var a = 10;
+// let b = 5;
+// let b = 10;
+let a = +prompt("Enter a value");
+let b = prompt("Enter b value");
+
+let c = a + b;
+console.log("The sum is "+c)
+console.log("welcome to Easy Language, Javascript")
+
+// Find area of rectangle using inputs length and breadth
+
+// write JS code to accept radius of a circle and print its area
+let r = +prompt("enter radius");
+const pi = 3.14;
+console.log("the area of circle " + (pi * r * r));
+
